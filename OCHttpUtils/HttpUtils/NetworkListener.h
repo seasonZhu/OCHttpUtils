a998afee87a919f6c29088aca13d86c2f491783a
zhujilong <zhujilong2007@sina.com>
2019-04-08 10:03:18 +0800

    添加注释

--- OCHttpUtils/HttpUtils/NetworkListener.h
+++ OCHttpUtils/HttpUtils/NetworkListener.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-/** 网络变化通知key*/
+/** 网络变化通知key 其实AFN中已经定义了一个监听网络变化的通知名,你完全可以直接使用它的通知名去进行监听,这里使用自己的,只是为了避免混淆*/
 FOUNDATION_EXPORT NSString * const NetworkDidChangedNotification;
 
 @interface NetworkListener : NSObject
